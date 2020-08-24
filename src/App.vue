15f3a77fcfde844b43ce5e6cd59a5325262f095b
pliavi <pliavi.rpgm@gmail.com>
2020-08-24 11:20:00 -0300

    create template

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <HelloWorld msg="Hello Vue 3.0 + Vite + TailwindCSS" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
